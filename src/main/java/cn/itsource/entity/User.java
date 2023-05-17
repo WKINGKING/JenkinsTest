e719cc039d4835e655909c8e1004bb9b9f2df379
wangkui <wangkui@asiainfo.com>
2023-05-17 16:31:46 +0800

    添加了redis

---
+++ src/main/java/cn/itsource/entity/User.java
@@ -0,0 +1,46 @@
+package cn.itsource.entity;
+
+import java.io.Serializable;
+
+/**
+ * @author wangkui
+ * @date 2023-05-17 14:17
+ * @description:
+ * @version:
+ */
+public class User implements Serializable {
+    private String name;
+    private int age;
+
+    public User(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public User() {
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
