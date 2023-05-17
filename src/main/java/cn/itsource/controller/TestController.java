dc904486f377477379b237c0d31532ed8b735a68
wangkui <wangkui@asiainfo.com>
2023-05-17 19:55:53 +0800

    数据库数据映射到前端

--- src/main/java/cn/itsource/controller/TestController.java
+++ src/main/java/cn/itsource/controller/TestController.java
@@ -1,6 +1,8 @@
 package cn.itsource.controller;
 
+import cn.itsource.service.UserServiceImpl;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 /**
@@ -11,12 +13,14 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @Slf4j
 public class TestController {
+    @Autowired
+    UserServiceImpl userService;
 
     @GetMapping("/test")
     public String test(){
         System.out.println("System.out.println-欢迎来到SpringBoot奇妙世界！");
         log.info("log.info-欢迎来到SpringBoot奇妙世界！");
-        return "欢迎来到SpringBoot奇妙世界！Jenkins学习";
+        return userService.queryAll().toString();
     }
 
 }
